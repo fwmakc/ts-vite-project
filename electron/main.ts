399de9c448c01e7b277bac46e380b08218ffcc17
fwmakc <fwmakcpro@yandex.ru>
2025-11-19 02:25:54 +0300

    modified:   electron/main.ts

--- electron/main.ts
+++ electron/main.ts
@@ -4,11 +4,15 @@ function createWindow(): void {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: true,
+      // devTools: false,
     },
-    autoHideMenuBar: true,
+    // autoHideMenuBar: true,
+    // fullscreen: true,
   });
 
   win.loadFile('dist/index.html');
