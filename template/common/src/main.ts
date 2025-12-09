1b2399fa4e9b3e4b51480c87ed62f9a949a4730e
fwmakc <fwmakcpro@yandex.ru>
2025-12-10 02:08:15 +0300

    update builds and subversion

--- template/common/src/main.ts
+++ template/common/src/main.ts
@@ -1,7 +1,11 @@
 import './assets/style.css';
+
+import { APP } from './app';
 import typescriptLogo from './assets/typescript.svg';
 import viteLogo from './assets/vite.svg';
 import { setupCounter } from './counter';
+
+console.log('🚀 Application is launched', APP);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -18,7 +22,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
-    <div class="card" id="license"></div>
+    <div class="card">${JSON.stringify(APP, null, ' ')}</div>
   </div>
 `;
 
