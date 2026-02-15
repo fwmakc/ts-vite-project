2f0990fc538880a9677fd2643f9a94c9c19086eb
fwmakc <fwmakcpro@yandex.ru>
2026-02-15 03:45:52 +0300

    fix: release config

---
+++ release.config.js
@@ -0,0 +1,65 @@
+export default {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'perf', release: 'patch' },
+          { type: 'chore', release: false },
+          { scope: 'no-release', release: false },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: '🚀 Новые функции', hidden: false },
+            { type: 'fix', section: '🐛 Исправления ошибок', hidden: false },
+            { type: 'perf', section: '⚡️ Оптимизация', hidden: false },
+            { type: 'revert', section: '⏪ Откаты изменений', hidden: false },
+            { type: 'docs', section: '📝 Документация', hidden: false },
+            { type: 'style', section: '💄 Стили и оформление', hidden: true },
+            { type: 'refactor', section: '♻️ Рефакторинг', hidden: false },
+            { type: 'test', section: '✅ Тесты', hidden: true },
+            { type: 'chore', section: '⚙️ Прочие задачи', hidden: true },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [{ path: 'dist/**/*.*' }, { path: 'CHANGELOG.md' }],
+        releasedLabels: ['Status: Released'],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
